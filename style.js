00671b897b3d6056e3053dbd1cb2a8d06a87a668
Trapgad <davidedemquarshie1@gmail.com>
2026-07-02 11:16:42 +0000

    Update style.js

--- style.js
+++ style.js
@@ -195,3 +195,9 @@ if(year){
 year.textContent = new Date().getFullYear();
 
 }
+const toggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+toggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
