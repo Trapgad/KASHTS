ee32de4d2e4e75bbb0de97767b5870c364ab25c3
Trapgad <davidedemquarshie1@gmail.com>
2026-07-02 12:20:08 +0000

    Update style.js

--- style.js
+++ style.js
@@ -219,3 +219,9 @@ const navMenu = document.getElementById("nav-menu");
 menuToggle.addEventListener("click", () => {
   navMenu.classList.toggle("active");
 });
+const menuToggle = document.getElementById("menu-toggle");
+const navMenu = document.getElementById("nav-menu");
+
+menuToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+});
